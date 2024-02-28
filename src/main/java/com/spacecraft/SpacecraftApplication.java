b7b4c43c34431205aa30d41b91de0832e667886a
OscarDRubio <oscardrm89@gmail.com>
2024-02-28 10:27:52 +0100

    Fix tests and passed lint

--- src/main/java/com/spacecraft/SpacecraftApplication.java
+++ src/main/java/com/spacecraft/SpacecraftApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableCaching
 @SpringBootApplication
-@EnableJpaRepositories("com.spacecraft.repository")
 @ComponentScan(basePackages = {"com.spacecraft.aspect", "com.spacecraft.repository", "com.spacecraft.controller",
         "com.spacecraft.model", "com.spacecraft.exception", "com.spacecraft.config"})
 public class SpacecraftApplication {
